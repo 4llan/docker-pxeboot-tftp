682ecd09708618614b86ec706fd4ec984cf3e28a
Allan M. de Azevedo <4lln.medeiros@gmail.com>
2020-06-17 16:31:14 -0300

    gen-pxeboot-cfg.sh optimizations
    - Reduction of if statements in some cases;
    - Passing version and arch of Clonezilla and Gparted calls

--- pxeboot/gen-pxeboot-cfg.sh
+++ pxeboot/gen-pxeboot-cfg.sh
@@ -5,34 +5,26 @@ TEMPDIR=$BASEDIR/tmp
 CFG_FILE=$TEMPDIR/pxeboot.cfg/default
 CFG_DIR=$(dirname $CFG_FILE)
 
-find $TEMPDIR -type f -not -name '*.zip' -delete
-find $TEMPDIR -type d -mindepth 1 -delete
-
 export PXE_IP_ADDR
 export PXE_TITLE=${PXE_TITLE:-"pxeboot@docker"}
 export PXE_PASSWD
 
 if [ -z $PXE_IP_ADDR ];
 then
-    echo "You need to set PXE_IP_ADDR env var"
+    echo "Environment variable PXE_IP_ADDR is not set"
     exit 2
 fi
 
 source "$BASEDIR/pxeboot-cfg.sh"
 
-if [[ ! -d $TEMPDIR ]];
-then
-    mkdir $TEMPDIR
-fi
-#cd $TEMPDIR
+find $TEMPDIR -type f -not -name '*.zip' -delete
+find $TEMPDIR -mindepth 1 -type d -delete
 
-if [[ ! -d $CFG_DIR ]];
-then
-    mkdir $CFG_DIR
-fi
+[ ! -d $TEMPDIR ] && mkdir $TEMPDIR
+[ ! -d $CFG_DIR ] && mkdir $CFG_DIR
 
 syslinux() {
-    local version=6.03
+    local version=${1:-"6.03"}
     local label=syslinux-$version
 
     set -- "bios/com32/menu/menu.c32" \
@@ -41,56 +33,52 @@ syslinux() {
     "bios/core/pxelinux.0"
 
     {
-        [ ! -f $TEMPDIR/$label.zip ] && wget -q https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/$label.zip -O $TEMPDIR/$label.zip
+        [ ! -f $TEMPDIR/$label.zip ] &&
+        wget -q https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/$label.zip -O $TEMPDIR/$label.zip
+
         unzip -q -o -j $TEMPDIR/$label "$@" -d $TEMPDIR
     } || return 255
 }
 
 memtest() {
-    local passwd=${1:-0}
-    local version=5.01
+    local passwd=${1:-0} && [ -z $PXE_PASSWD ] && passwd=0
+    local version=${2:-"5.01"}
     local label=memtest86+-$version
     local kernel=/$label/$label
-    if [ -z $PXE_PASSWD ];
-    then
-        passwd=0
-    fi
 
-    if [ ! -d $TEMPDIR/$label ];
-    then
-        mkdir $TEMPDIR/$label
-    fi
     {
-        [ ! -f $TEMPDIR/$label.zip ] && wget -q http://www.memtest.org/download/$version/$label.zip -O $TEMPDIR/$label.zip
+        [ ! -d $TEMPDIR/$label ] &&
+        mkdir $TEMPDIR/$label
+
+        [ ! -f $TEMPDIR/$label.zip ] &&
+        wget -q http://www.memtest.org/download/$version/$label.zip -O $TEMPDIR/$label.zip
+
         unzip -q -o $TEMPDIR/$label.zip -d $TEMPDIR/$label &&
-        mv -f $TEMPDIR/$label/$label.bin $TEMPDIR/$label/$label
+        mv -f $TEMPDIR/$label/*.bin $TEMPDIR/$label/$label
     } || return 255
 
     label $label "Memtest86+ $version" $kernel "" $passwd
 }
 
 clonezilla() {
-    local passwd=${1:-0}
-    local version=2.6.6-15 #2.5.6-22
-    local arch=amd64
+    local passwd=${1:-0} && [ -z $PXE_PASSWD ] && passwd=0
+    local version=${2:-"2.5.6-22"}
+    local arch=${3:-"amd64"}
     local label=clonezilla-live-$version-$arch
     local kernel=/$label/vmlinuz
     local append="initrd=$label/initrd.img boot=live username=user union=overlay config components quiet noswap edd=on nomodeset nodmraid locales= keyboard-layouts= ocs_live_run=\"ocs-live-general\" ocs_live_extra_param=\"\" ocs_live_batch=no net.ifnames=0 nosplash noprompt vga=788 fetch=tftp://$PXE_IP_ADDR/$label/filesystem.squashfs"
-    if [ -z $PXE_PASSWD ];
-    then
-        passwd=0
-    fi
 
     set -- "live/vmlinuz" \
     "live/initrd.img" \
     "live/filesystem.squashfs"
 
-    if [ ! -d $TEMPDIR/$label ];
-    then
-        mkdir $TEMPDIR/$label
-    fi
     {
-        [ ! -f $TEMPDIR/$label.zip ] && wget -q https://sourceforge.net/projects/clonezilla/files/clonezilla_live_stable/$version/$label.zip/download -O $TEMPDIR/$label.zip
+        [ ! -d $TEMPDIR/$label ] &&
+        mkdir $TEMPDIR/$label
+
+        [ ! -f $TEMPDIR/$label.zip ] &&
+        wget -q https://sourceforge.net/projects/clonezilla/files/clonezilla_live_stable/$version/$label.zip/download -O $TEMPDIR/$label.zip
+
         unzip -q -o -j $TEMPDIR/$label.zip "$@" -d $TEMPDIR/$label
     } || return 255
 
@@ -98,27 +86,24 @@ clonezilla() {
 }
 
 gparted() {
-    local passwd=${1:-0}
-    local version=1.1.0-1 #0.33.0-1
-    local arch=amd64
+    local passwd=${1:-0} && [ -z $PXE_PASSWD ] && passwd=0
+    local version=${2:-"0.33.0-1"}
+    local arch=${3:-"amd64"}
     local label=gparted-live-$version-$arch
     local kernel=/$label/vmlinuz
     local append="initrd=$label/initrd.img boot=live config components union=overlay username=user noswap noeject ip= vga=788 fetch=tftp://$PXE_IP_ADDR/$label/filesystem.squashfs"
-    if [ -z $PXE_PASSWD ];
-    then
-        passwd=0
-    fi
 
     set -- "live/vmlinuz" \
     "live/initrd.img" \
     "live/filesystem.squashfs"
 
-    if [ ! -d $TEMPDIR/$label ];
-    then
-        mkdir $TEMPDIR/$label
-    fi
     {
-        [ ! -f $TEMPDIR/$label.zip ] && wget -q https://sourceforge.net/projects/gparted/files/gparted-live-stable/$version/$label.zip/download -O $TEMPDIR/$label.zip
+        [ ! -d $TEMPDIR/$label ] &&
+        mkdir $TEMPDIR/$label
+
+        [ ! -f $TEMPDIR/$label.zip ] &&
+        wget -q https://sourceforge.net/projects/gparted/files/gparted-live-stable/$version/$label.zip/download -O $TEMPDIR/$label.zip
+
         unzip -q -o -j $TEMPDIR/$label.zip "$@" -d $TEMPDIR/$label
     } || return 255
 
@@ -129,8 +114,8 @@ main() {
     syslinux &&
     header "$PXE_TITLE" "$PXE_PASSWD" &&
     memtest 1 &&
-    clonezilla 1 &&
-    gparted 1
+    clonezilla 1 "2.6.6-15" "amd64" &&
+    gparted 1 "1.1.0-1" "amd64"
 }
 
 main > $CFG_FILE || {
