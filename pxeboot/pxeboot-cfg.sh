63bf525961b45a76f6c4286bb15a54086e645244
Allan M. de Azevedo <4lln.medeiros@gmail.com>
2020-07-15 17:09:14 -0300

    Add comments to pxeboot-cfg.sh

--- pxeboot/pxeboot-cfg.sh
+++ pxeboot/pxeboot-cfg.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
+#
+# Set functions to generate menu header and labels according to Syslinux Menu system.
+# https://wiki.syslinux.org/wiki/index.php?title=Config
+# https://wiki.syslinux.org/wiki/index.php?title=Menu
+# https://wiki.syslinux.org/wiki/index.php?title=Comboot/menu.c32
+
 if [ ${0##*/} = "pxeboot-cfg.sh" ];
 then
     echo "This script is meant to be sourced"
     exit 1
 fi
 
+#######################################
+# Echoes the Syslinux menu header.
+# Arguments:
+#   [$1] Menu title, string
+#   [$2] Master password (plaintext or hash), string
+#######################################
 header() {
     echo "DEFAULT menu.c32"
     echo "PROMPT 0"
@@ -16,6 +28,15 @@ header() {
     fi
 }
 
+#######################################
+# Echoes the Syslinux menu entry label.
+# Arguments:
+#   [$1] Label name, string
+#   [$2] Label displayed, string
+#   [$3] Kernel-like file image, string (relative path)
+#   [$4] Append kernel options, string
+#   [$5] Password protected, int (1 == ON)
+#######################################
 label() {
     if [ "$1" ] && [ "$2" ] && [ "$3" ];
     then
